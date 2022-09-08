c0f9579ca13a181e6226409f59171fe79054c5d2
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 18:05:53 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -9,5 +9,8 @@ conn = snowflake.connector.connect(
                 account='bv18063.ap-southeast-1',
     ocsp_fail_open=False
                 )
-
+db = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
+dbs = list(set(list(db['DATABASE'])))
+option = st.selectbox('select database:',dbs)
+st.write('Selected Database :', option)
 
