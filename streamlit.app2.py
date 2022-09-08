730f9740ac2411e4403ab570905595d5041429c4
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 18:04:48 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -3,6 +3,11 @@ st.title("Snowflake Data-App")
 
 import pandas as pd
 import snowflake.connector
-
+conn = snowflake.connector.connect(
+                user='VARSHINI',
+                password='Snowflake@22!',
+                account='bv18063.ap-southeast-1',
+    ocsp_fail_open=False
+                )
 
 
