6b84867df9a1afdf85469b60cd18a26adabfc943
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 17:55:06 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import snowflake.connector
-
 conn = snowflake.connector.connect(
                 user='VARSHINI',
                 password='Snowflake@22!',
