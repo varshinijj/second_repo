20718420c24677c37f3b57d57e13b87e7ed6b120
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 18:17:49 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -14,3 +14,24 @@ dbs = list(set(list(db['DATABASE'])))
 option = st.selectbox('select database:',dbs)
 st.write('Selected Database :', option)
 
+sc= pd.read_sql("select schema_name as schema,catalog_name as database from SNOWFLAKE.ACCOUNT_USAGE.SCHEMATA where deleted is NULL and catalog_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn) 
+scl = sc.loc[sc['DATABASE']==option].reset_index(drop=True)
+scl
+scs = list(set(list(scl['SCHEMA'])))
+next = st.selectbox('select schema:',scs)
+st.write('Selected Schema:', next)
+
+tab = pd.read_sql("select table_name,table_schema as schema,table_catalog as database from SNOWFLAKE.ACCOUNT_USAGE.TABLES where deleted is NULL and table_catalog not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
+tabl1 = tab.loc[tab['SCHEMA']==next]
+tabl2 = tabl1.loc[tabl1['DATABASE']==option].reset_index(drop=True)
+tabl2
+tabs = list(set(list(tabl2['TABLE_NAME'])))
+final = st.selectbox('select table:',tabs)
+st.write('Selected Table:', final)
+
+dis = pd.read_sql("select COLUMN_NAME,TAG_NAME,TAG_VALUE from table({}.information_schema.tag_references_all_columns('{}.{}.{}', 'table'));".format(option,option,next,final)
+
+disp_pivot=dis.pivot(index=['COLUMN_NAME'],columns=['TAG_NAME'],values=['TAG_VALUE']).reset_index()
+disp_pivot
+
+
