1fc4a2266762de8b43cad03a11a215cd4e6e28dc
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 17:59:14 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -3,17 +3,4 @@ st.title("Snowflake Data-App")
 
 import pandas as pd
 
-import snowflake.connector
-conn = snowflake.connector.connect(
-                user='VARSHINI',
-                password='Snowflake@22!',
-                account='bv18063.ap-southeast-1',
-    ocsp_fail_open=False
-                )
-                
-st.title("Snowflake Data-App") 
 
-db = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
-dbs = list(set(list(db['DATABASE'])))
-option = st.selectbox('select database:',dbs)
-st.write('Selected Database :', option)
