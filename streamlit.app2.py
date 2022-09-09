32707c4a0b0f3b34bb0a77d8d5f448c2a9753adc
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 12:05:34 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import pandas as pd
 import graphviz
+st.graphviz_chart(d)
+tabd = pd.DataFrame(columns=['db', 'sc', 'tab'])
+tabd['db'] = ['db1','db1','db1','db1','db1','db1','db2','db2','db2','db2','db2','db2','db3','db3','db3','db3','db3','db3']
+tabd['sc'] = ['db1s1','db1s1','db1s2','db1s2','db1s3','db1s3','db2s1','db2s1','db2s2','db2s2','db2s3','db2s3','db3s1','db3s1','db3s2','db3s2','db3s3','db3s3']
+tabd['tab'] = ['tab111','tab112','tab121','tab122','tab131','tab132','tab211','tab212','tab221','tab222','tab231','tab232','tab311','tab312','tab321','tab322','tab331','tab332']
+tabd
 
-
+db = st.sidebar.selectbox("choose db:",tabd['db'])
 d = graphviz.Digraph()
 
 with d.subgraph() as s:
     s.attr(rank='same')
-    s.node('A')
-    s.node('X')
+    s.node('{}'.format(db))
+    
 
 d.node('C')
 
@@ -19,15 +25,6 @@ with d.subgraph() as s:
     s.node('Y')
 
 d.edges(['AB', 'AC', 'CD', 'XY'])
-
-st.graphviz_chart(d)
-tabd = pd.DataFrame(columns=['db', 'sc', 'tab'])
-tabd['db'] = ['db1','db1','db1','db1','db1','db1','db2','db2','db2','db2','db2','db2','db3','db3','db3','db3','db3','db3']
-tabd['sc'] = ['db1s1','db1s1','db1s2','db1s2','db1s3','db1s3','db2s1','db2s1','db2s2','db2s2','db2s3','db2s3','db3s1','db3s1','db3s2','db3s2','db3s3','db3s3']
-tabd['tab'] = ['tab111','tab112','tab121','tab122','tab131','tab132','tab211','tab212','tab221','tab222','tab231','tab232','tab311','tab312','tab321','tab322','tab331','tab332']
-tabd
-
-st.sidebar.selectbox("choose db:",tabd['db'])
 
 
 
