7a4db747833929b3fb154ce22a0bf8b28e00e70c
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 11:48:09 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import graphviz
 
+
 d = graphviz.Digraph()
 
 with d.subgraph() as s:
@@ -20,6 +21,11 @@ with d.subgraph() as s:
 d.edges(['AB', 'AC', 'CD', 'XY'])
 
 st.graphviz_chart(d)
+salgrade = pd.DataFrame(columns=['GRADE', 'LOSAL', 'HISAL'])
+salgrade['GRADE'] = [1,2,3,4,5]
+salgrade['LOSAL'] = [700,1201,1401,2001,3001]
+salgrade['HISAL'] = [1200,1400,2000,3000,9999]
+salgrade
 
 
 
