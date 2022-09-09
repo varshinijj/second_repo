832453b3eec5bf0a9ffe7a79b292067586e4b1d7
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 12:07:56 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import graphviz
-st.graphviz_chart(d)
+
 tabd = pd.DataFrame(columns=['db', 'sc', 'tab'])
 tabd['db'] = ['db1','db1','db1','db1','db1','db1','db2','db2','db2','db2','db2','db2','db3','db3','db3','db3','db3','db3']
 tabd['sc'] = ['db1s1','db1s1','db1s2','db1s2','db1s3','db1s3','db2s1','db2s1','db2s2','db2s2','db2s3','db2s3','db3s1','db3s1','db3s2','db3s2','db3s3','db3s3']
@@ -27,7 +27,7 @@ with d.subgraph() as s:
 
 d.edges(['AB', 'AC', 'CD', 'XY'])
 
-
+st.graphviz_chart(d)
 
 
 
