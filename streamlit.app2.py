fd0a6c4216050d307f9114b500e9a75cd3d508fa
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 17:50:53 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -19,15 +19,14 @@ with d.subgraph() as s:
     s.attr(rank='same')
     for x in list(sc['sc'].unique()):
         s.node('{}'.format(st.button('{}'.format(x))))
-        
+        d.node('{}'.format(db),'{}'.format(x))
 with d.subgraph() as s:
     s.attr(rank='same')
     for idx,row in sc.iterrows():
         s.node('{}'.format(row['tab']))
-        d.edge('{}'.format(row['sc']),'{}'.format(row['tab']))  
+        d.edge('{}'.format(row['sc']),'{}'.format(row['tab'])) 
         
       
-
 st.graphviz_chart(d)
 
 
