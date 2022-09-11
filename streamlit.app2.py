77c2319e6cc38b0f81b16236ce0278c8361b29f8
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-11 13:29:33 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import graphviz
-st.set_page_config(layout="wide",color='dark')
+st.set_page_config(layout="wide")
 tabd = pd.DataFrame(columns=['db', 'sc', 'tab'])
 tabd['db'] = ['db1','db1','db1','db1','db1','db1','db2','db2','db2','db2','db2','db2','db3','db3','db3','db3','db3','db3']
 tabd['sc'] = ['db1s1','db1s1','db1s2','db1s2','db1s3','db1s3','db2s1','db2s1','db2s2','db2s2','db2s3','db2s3','db3s1','db3s1','db3s2','db3s2','db3s3','db3s3']
