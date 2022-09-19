39418008091dccd1d8748aefeef3d77cc7d15438
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-20 02:32:15 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -29,7 +29,7 @@ with col1:
                 
 with col2:                
     d = graphviz.Digraph()
-    d.attr(bgcolor='#0e1117',orientation="landscape")
+    d.attr(bgcolor='#0e1117')
     with d.subgraph() as s:
         s.attr(rank='same')
         s.node('{}'.format(db), fontcolor='white',color = 'red')  
