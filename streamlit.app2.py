32d42f705116f306a5f138d616b74a45bade950d
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-20 00:09:57 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -5,44 +5,30 @@ st.set_page_config(layout="wide")
 
         
 from graphviz import Digraph    
-
-tabd = pd.DataFrame(columns=['db', 'sc', 'tab'])
-tabd['db'] = ['db1','db1','db1','db1','db1','db1','db2','db2','db2','db2','db2','db2','db3','db3','db3','db3','db3','db3']
-tabd['sc'] = ['db1s1','db1s1','db1s2','db1s2','db1s3','db1s3','db2s1','db2s1','db2s2','db2s2','db2s3','db2s3','db3s1','db3s1','db3s2','db3s2','db3s3','db3s3']
-tabd['tab'] = ['tab111','tab112','tab121','tab122','tab131','tab132','tab211','tab212','tab221','tab222','tab231','tab232','tab311','tab312','tab321','tab322','tab331','tab332']
-
-db = st.sidebar.selectbox("choose db:",tabd['db'])
-sc = tabd.loc[tabd['db']==db][['sc','tab']]
-
-col1, col2 = st.columns([1, 4])
-
-
-with col1:
-    sel = ['All Schemas','Select Schemas']
-    click = st.radio('Schemas:',sel)
-    if click =='All Schemas':
-        sc = tabd.loc[tabd['db']==db][['sc','tab']] 
-    else:
-        for x in list(sc['sc'].unique()): 
-            schemas = st.checkbox('{}'.format(x),False,disabled=False)
-            if schemas==False:
-                sc = sc.loc[sc['sc']!=x]
-   
-        
-        
-with col2:   
-    d = graphviz.Digraph()
-    d.attr(bgcolor='#0e1117')
-    with d.subgraph() as s:
-        s.attr(rank='same')
-        s.node('{}'.format(db), fontcolor='white',color = 'white')  
-    G = graphviz.Digraph(        
-            node['{}'.format(db)] 
-            )
-
-
-    st.graphviz_chart(d)
-    st.graphviz_chart(G)    
+digraph G {
+subgraph cluster0 {
+node [style=filled,color=white];
+style=filled;
+color=lightgrey;
+a0 -> a1 -> a2 -> a3;
+label = "process #1";
+}
+subgraph cluster1 {
+node [style=filled];
+b0 -> b1 -> b2 -> b3;
+label = "process #2";
+color=blue
+}
+start -> a0;
+start -> b0;
+a1 -> b3;
+b2 -> a3;
+a3 -> a0;
+a3 -> end;
+b3 -> end;
+start [shape=Mdiamond];
+end [shape=Msquare];
+}
 
 
 
