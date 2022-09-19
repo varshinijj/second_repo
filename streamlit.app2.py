8ac7c05ce6969ad2c6c04124c46f6cd795452eab
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-20 00:20:04 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -5,12 +5,16 @@ st.set_page_config(layout="wide")
 from graphviz import Digraph    
 
 st.graphviz_chart('''
-    digraph {
-        Big_shark -> Tuna
-        Tuna -> Mackerel
-        Mackerel -> Small_fishes
-        Small_fishes -> Shrimp
-    }
+    digraph ok {
+  subgraph cluster_0{
+     { rank=same  // all nodes on same rank
+       node [shape ="rectangle"]    // for all nodes in this subgraph
+       edge [style=invis]           // for all edges, invisible links
+       // we use the invisible edges to establish their sequence (kludge)
+       name0 ->  name1 ->  name2 
+     }
+  }
+}
 ''')
 
 
