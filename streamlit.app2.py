367aef2b027690eb882286b0d1fd5867f9cbdecf
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-19 22:38:15 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -2,56 +2,40 @@ import streamlit as st
 import pandas as pd
 import graphviz
 st.set_page_config(layout="wide")
-tabd = pd.DataFrame(columns=['db', 'sc', 'tab'])
-tabd['db'] = ['db1','db1','db1','db1','db1','db1','db2','db2','db2','db2','db2','db2','db3','db3','db3','db3','db3','db3']
-tabd['sc'] = ['db1s1','db1s1','db1s2','db1s2','db1s3','db1s3','db2s1','db2s1','db2s2','db2s2','db2s3','db2s3','db3s1','db3s1','db3s2','db3s2','db3s3','db3s3']
-tabd['tab'] = ['tab111','tab112','tab121','tab122','tab131','tab132','tab211','tab212','tab221','tab222','tab231','tab232','tab311','tab312','tab321','tab322','tab331','tab332']
 
-db = st.sidebar.selectbox("choose db:",tabd['db'])
-sc = tabd.loc[tabd['db']==db][['sc','tab']]
-
-col1, col2 = st.columns([1, 4])
-
-
-with col1:
-    sel = ['All Schemas','Select Schemas']
-    click = st.radio('Schemas:',sel)
-    if click =='All Schemas':
-        sc = tabd.loc[tabd['db']==db][['sc','tab']] 
-    else:
-        for x in list(sc['sc'].unique()): 
-            schemas = st.checkbox('{}'.format(x),False,disabled=False)
-            if schemas==False:
-                sc = sc.loc[sc['sc']!=x]
         
     
     
     
  
-with col2:
-  digraph cats {
-    subgraph cluster_big_cats {
-      // This subgraph is a cluster, because the name begins with "cluster"
+
+
+g = Digraph(node_attr={'shape':'box', 'style': 'rounded,filled'})
+
+# NOTE: the subgraph name needs to begin with 'cluster' (all lowercase)
+#       so that Graphviz recognizes it as a special cluster subgraph
+
+with g.subgraph(name='cluster_A',) as c:
+    c.attr(shape='box', style= 'rounded,filled', color='#82d2f0', label='Cluster A')
+    c.attr('node', color='blue',)
+    c.node('Node A')
+    c.node('Node B')
     
-      "Lion";
-      "Snow Leopard";
-    }
-
-    subgraph domestic_cats {
-      // This subgraph is also a cluster, because cluster=true.
-      cluster=true;
-
-      "Siamese";
-      "Persian";
-    }
-
-    subgraph not_a_cluster {
-      // This subgraph is not a cluster, because it doesn't start with "cluster",
-      // nor sets cluster=true.
+with g.subgraph(name='cluster_B') as a:
+    a.attr(shape='box', style= 'rounded,filled', color='#fac800', label='Cluster B')
+    a.attr('node', color='orange')
+    a.node('Node C')
+    a.node('Node D')
     
-      "Wildcat";
-    }
-  }
+    with a.subgraph(name='cluster_B_1') as b:
+        b.attr(shape='box', style= 'rounded,filled', color='yellow', label='Cluster B 1')
+        b.node('Node E')
+        b.node('Node F')
+
+
+
+g.edge('cluster_B_1', 'Node A')
+g.render('GraphvizTest', cleanup=True)
 
 
 
