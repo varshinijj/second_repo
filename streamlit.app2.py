7b2b0765e63269dc7e431afe7c3b24fcdbf104ae
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-19 22:36:45 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -29,13 +29,29 @@ with col1:
     
  
 with col2:
-    c = graphviz.digraph {rankdir=LR;
-        node [ shape=record ];
-        struct1 [
-            label = "a|b|<port1>c";
-        ];
-       }
-    st.graphviz_chart(c)
+  digraph cats {
+    subgraph cluster_big_cats {
+      // This subgraph is a cluster, because the name begins with "cluster"
+    
+      "Lion";
+      "Snow Leopard";
+    }
+
+    subgraph domestic_cats {
+      // This subgraph is also a cluster, because cluster=true.
+      cluster=true;
+
+      "Siamese";
+      "Persian";
+    }
+
+    subgraph not_a_cluster {
+      // This subgraph is not a cluster, because it doesn't start with "cluster",
+      // nor sets cluster=true.
+    
+      "Wildcat";
+    }
+  }
 
 
 
