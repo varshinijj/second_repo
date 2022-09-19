aa26e3f62232ea9ad5a3fbd9bcf348074722888a
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-20 01:15:49 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -40,10 +40,10 @@ with col2:
             s.node('{}'.format(x), fontcolor='white',color = 'red')
             d.edge('{}'.format(db),'{}'.format(x),color = 'white')
     with d.subgraph() as s:
-        s.attr(rank='same')
+        
         for idx,row in sc.iterrows():
             s.node('{}'.format(row['tab']), fontcolor='white',color = 'red')
-            d.edge('{}'.format(row['sc']),'{}'.format(row['tab']),color = 'white')        
+            d.edge('{}'.format(row['sc']),'{}'.format(row['tab']),color = 'white')       
     st.graphviz_chart(d)
 
 
