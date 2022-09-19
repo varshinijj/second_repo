7fa54cc64bad1e75ea317eb2c510ce12fa6124c7
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-19 22:40:34 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -5,37 +5,23 @@ st.set_page_config(layout="wide")
 
         
 from graphviz import Digraph    
-    
-    
- 
+g = graphviz.Graph('G', filename='fdpclust.gv', engine='fdp')
 
+g.node('e')
 
-g = Digraph(node_attr={'shape':'box', 'style': 'rounded,filled'})
+with g.subgraph(name='clusterA') as a:
+    a.edge('a', 'b')
+    with a.subgraph(name='clusterC') as c:
+        c.edge('C', 'D')
 
-# NOTE: the subgraph name needs to begin with 'cluster' (all lowercase)
-#       so that Graphviz recognizes it as a special cluster subgraph
+with g.subgraph(name='clusterB') as b:
+    b.edge('d', 'f')
 
-with g.subgraph(name='cluster_A',) as c:
-    c.attr(shape='box', style= 'rounded,filled', color='#82d2f0', label='Cluster A')
-    c.attr('node', color='blue',)
-    c.node('Node A')
-    c.node('Node B')
-    
-with g.subgraph(name='cluster_B') as a:
-    a.attr(shape='box', style= 'rounded,filled', color='#fac800', label='Cluster B')
-    a.attr('node', color='orange')
-    a.node('Node C')
-    a.node('Node D')
-    
-    with a.subgraph(name='cluster_B_1') as b:
-        b.attr(shape='box', style= 'rounded,filled', color='yellow', label='Cluster B 1')
-        b.node('Node E')
-        b.node('Node F')
+g.edge('d', 'D')
+g.edge('e', 'clusterB')
+g.edge('clusterC', 'clusterB')
 
-
-
-g.edge('cluster_B_1', 'Node A')
-g.render('GraphvizTest', cleanup=True)
+g.view()
 
 
 
