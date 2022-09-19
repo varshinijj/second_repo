dba83e8ed4aab3b18f8ea770bdd4ce44a4f2da8f
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-20 00:18:04 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -1,34 +1,14 @@
 import streamlit as st
 import pandas as pd
 import graphviz
-st.set_page_config(layout="wide")
-
-        
+st.set_page_config(layout="wide")        
 from graphviz import Digraph    
-digraph G {
-subgraph cluster0 {
-node [style=filled,color=white];
-style=filled;
-color=lightgrey;
-a0 -> a1 -> a2 -> a3;
-label = "process #1";
+
+
+digraph {
+  abc [color = red]
 }
-subgraph cluster1 {
-node [style=filled];
-b0 -> b1 -> b2 -> b3;
-label = "process #2";
-color=blue
-}
-start -> a0;
-start -> b0;
-a1 -> b3;
-b2 -> a3;
-a3 -> a0;
-a3 -> end;
-b3 -> end;
-start [shape=Mdiamond];
-end [shape=Msquare];
-}
+
 
 
 
