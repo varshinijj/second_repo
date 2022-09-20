f488fe4ea59505c9ec4c83d1ec9f6abd2a967943
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-20 14:57:30 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -45,6 +45,7 @@ with col2:
             lala= sc.loc[sc['sc']==row['sc']][['tab']]
             lala = lala.reset_index(drop=True)
             lala.rename(columns = {'tab':'TABLE_NAME'}, inplace = True)
+            lala = lala.set_index('TABLE_NAME')
             s.node('{}'.format(pd.DataFrame(lala)),shape='tab', fontcolor='white',color = 'red')
             d.edge('{}'.format(row['sc']),'{}'.format(pd.DataFrame(lala)),color='white')
             sl.append(row['sc'])
