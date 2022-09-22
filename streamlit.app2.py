a4a35210e029e068266755149f89452f75dadc11
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-22 22:13:24 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -19,6 +19,7 @@ cur = conn.cursor()
 ####database selection####
 
 ##DB
+@st.experimental_singleton
 def all_databases():
   db_data = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA') and deleted is null;",conn)
   dbs = list(set(db_data['DATABASE']))
